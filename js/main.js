45aae5d1e2feb9a033be61ee3f93c7434abffb86
kyunghee-oh <rudgml2141@naver.com>
2019-11-12 01:41:35 +0900

    add-code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
 
-    //맨뒤 한칸 앞으로 빼두기
   $(".slide-wrapper .slide-wrapper-item li:last").prependTo(".slide-wrapper .slide-wrapper-item");
   $(".slide-wrapper .slide-wrapper-item").css({"marginLeft":"-100%"});
     //다음버튼 클릭시
   $(".ctrol-btn .next").click(function(){
     $(".slide-wrapper .slide-wrapper-item").animate({"marginLeft":"-=100%"} , "fast", function(){
       $(".slide-wrapper .slide-wrapper-item li:first").appendTo(".slide-wrapper .slide-wrapper-item");
-      $(".slide-wrapper .slide-wrapper-item").css({"marginLeft":"-100%"}); // 한번 움직일때마다 순서 바꾸고 앞으로 기차칸빼기 가동
+      $(".slide-wrapper .slide-wrapper-item").css({"marginLeft":"-100%"});
     });
 
   });
@@ -15,27 +14,12 @@ $(document).ready(function(){
   $(".ctrol-btn .prev").click(function(){
     $(".slide-wrapper .slide-wrapper-item").animate({"marginLeft":"+=100%"} , "fast", function(){
       $(".slide-wrapper .slide-wrapper-item li:last").prependTo(".slide-wrapper .slide-wrapper-item");
-      $(".slide-wrapper .slide-wrapper-item").css({"marginLeft":"-100%"}); // 한번 움직일때마다 순서 바꾸고 앞으로 기차칸빼기 가동
+      $(".slide-wrapper .slide-wrapper-item").css({"marginLeft":"-100%"});
     });
 
   });
 
-
-// My skills
-  // $(function(){
-  //
-  //   var $content2about = $('.content2-about');
-  //   var $offset = 1400;
-  //   var $content2aboutOST = $content2_about.offset().top - $offset;
-  //
-  //   $(window).scroll(function(){
-  //     if($(this).scrollTop() > $content2aboutOST) {
-  //       $content2about.find('.chart-wrap').addClass('animate');
-  //     }
-  //
-  //   });
-  //
-  // });
+  // My skill
   $(function(){
     $(window).scroll(function(){
       $('.root-box').each( function(i){
--- js/sub.js
+++ js/sub.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
   // Other Pages
   $(function(){
-    //클릭하기 전 그림
-    //클릭했을때 바뀔 그림 두개 겹치기(깜박거리는 이유는 두개 안겹쳐놔서)
     $(".list-btn-box li a ").click(function(){
       $(".slide-tab img").before('<img src="'+ $(this).attr("href") +'" alt=""/>');
       $(".slide-tab img:last").fadeOut(500, function(){
